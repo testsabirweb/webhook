0206815a1ecbf3ed0b0b8444a047cef328397265
Sabir Ali <saali@purestorage.com>
2024-09-20 09:44:04 +0000

    https setup

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	if err := r.RunTLS(":8080", "/etc/certs/tls.crt", "/etc/certs/tls.key"); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+
+	log.Println("Starting server https://localhost:8080/")
 }
